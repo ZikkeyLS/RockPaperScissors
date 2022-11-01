76091dbeb1d392094c66c2b34b1eb44aa32db50b
ZikkeyLS <hisaha21@yandex.ru>
2022-11-01 18:21:49 +0300

    Upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RockPaperScissors.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
